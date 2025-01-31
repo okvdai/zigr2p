bc8f2cb2d90a461408c7e54f221d150873598585
okvdai <108829133+okvdai@users.noreply.github.com>
2025-01-31 17:30:35 +0100

    style: add missing comma

--- src/squirrel.zig
+++ src/squirrel.zig
@@ -9,7 +9,28 @@ pub const Type = struct {
     pub const Char = c_char;
     pub const Bool = Type.UInt;
 };
-pub const SQObjType = enum(c_int) { userptr = 0x800, vector = 0x40000, null = 0x1000001, bool = 0x1000008, int = 0x5000002, float = 0x5000004, string = 0x8000010, array = 0x8000040, closure = 0x8000100, nativeclosure = 0x8000200, asset = 0x8000400, thread = 0x8001000, protofn = 0x8002000, class = 0x8004000, strct = 0x8200000, weakref = 0x8010000, table = 0xA000020, userdata = 0xA000080, instance = 0xA008000, entity = 0xA400000 };
+pub const SQObjType = enum(c_int) {
+    userptr = 0x800,
+    vector = 0x40000,
+    null = 0x1000001,
+    bool = 0x1000008,
+    int = 0x5000002,
+    float = 0x5000004,
+    string = 0x8000010,
+    array = 0x8000040,
+    closure = 0x8000100,
+    nativeclosure = 0x8000200,
+    asset = 0x8000400,
+    thread = 0x8001000,
+    protofn = 0x8002000,
+    class = 0x8004000,
+    strct = 0x8200000,
+    weakref = 0x8010000,
+    table = 0xA000020,
+    userdata = 0xA000080,
+    instance = 0xA008000,
+    entity = 0xA400000,
+};
 pub const SQObj = extern struct {
     Type: SQObjType,
     StructNumber: c_int,
